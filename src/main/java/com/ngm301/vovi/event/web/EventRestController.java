435be1f884c07a86bd44757ecffe984169fdc248
sgjang11 <sgjang11@outlook.kr>
2023-12-06 00:02:57 +0900

    feat: first commit

---
+++ src/main/java/com/ngm301/vovi/event/web/EventRestController.java
@@ -0,0 +1,10 @@
+package com.ngm301.vovi.event.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "event")
+@RestController
+public class EventRestController {
+
+}
---
+++ src/main/resources/static/vovi/event/js/event.js
@@ -0,0 +1,16 @@
+/**
+ * @Class Name : vovi/event/js/event.js
+ * @Description : event js
+ * @Modification : Information
+ * 
+ * 수정일				수정자			수정내용
+ * -------------------------------------------------------
+ * 2023-12-04		slaptear	최초 생성
+ * 
+ * author: slaptear
+ * since 2023-12-04 MON
+ */
+$(document).ready(function(){
+	console.log("어서오시와요");
+})
+

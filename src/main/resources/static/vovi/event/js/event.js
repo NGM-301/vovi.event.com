0cfba876e83302315514d4f8f747bc0240266da4
sgjang11 <sgjang11@outlook.kr>
2023-12-17 02:01:17 +0900

    fix: 디자인 수정

--- src/main/resources/static/vovi/event/js/event.js
+++ src/main/resources/static/vovi/event/js/event.js
@@ -41,29 +41,43 @@ function fn_showAddressView(){
 	alert("test");
 }
 
+function fn_toggleSaveBtn(){
+	$("#reviewEventCard").show();
+	const MOD_BTN = `<button type="button" class="btn btn-outline-warning modBtn">수정</button>`;
+	$("#reviewerInfoCard .saveBtn").remove();
+	$("#reviewerInfoCard .card-header").append(MOD_BTN);
+	$("#reviewerInfoCard input").attr("readOnly", true);
+}
 
-$('#saveAndModBtn').click(function() {
-	btnCount = 1;
-	$(this).addClass("saveReviewer");
-	$("#reviewerInfoCard input").attr("readonly",true);
-	if ($(this).hasClass("saveReviewer")) {
-		btnCount = 0;
-		$("#reviewEventCard").show();
-		$(this).removeClass('btn-outline-success').addClass('btn-outline-warning');
-		$(this).text("수정");
-	}
+function fn_toggleModBtn(){
+	$("#reviewEventCard").hide();
+	const SAVE_BTN = `<button type="button" class="btn btn-outline-success saveBtn">저장</button>`;
+	$("#reviewerInfoCard .modBtn").remove();
+	$("#reviewerInfoCard input").attr("readOnly", false);
+	$("#reviewerInfoCard .card-header").append(SAVE_BTN);
+}
+
+$('#reviewerInfoCard').on('click', '.saveBtn', function() {
+	fn_toggleSaveBtn();
+});
+
+$('#reviewerInfoCard').on('click', '.modBtn', function() {
+	fn_toggleModBtn();
 });
 
 $('#checkAgree').change(function() {
 	if ($(this).is(':checked')) {
 		$("#reviewerInfoCard").show();
+		fn_toggleModBtn();
 	} else {
 		$("#reviewerInfoCard").hide();
 		$("#reviewEventCard").hide();
 	}
 });
 
-
+$("#eventParticipation").click(function (){
+	alert("참여버튼")
+})
 
 /* 초기화 */
 //fn_get("event/member", {userNm : "김선호", userTelNo: "010-0000-0000"});

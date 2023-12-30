a50ebfe9feff755a8104263312e2ea55b70abfdf
sgjang11 <sgjang11@outlook.kr>
2023-12-30 22:48:44 +0900

    fix: 디자인 수정

--- src/main/resources/static/vovi/common/js/common.js
+++ src/main/resources/static/vovi/common/js/common.js
@@ -92,6 +92,227 @@ function deleteLoadingSpinner($cardDivList)
 	})
 }
 
+/* ---- confirm 알림 함수 ---- */
+/*  
+사용예시)
+modalConfirm("추천키워드를 추가하시겠습니까?").then((result) => {
+	if (result) {
+		// true일 경우 실행할 함수
+	}else{
+		// false일 경우 실행할 함수
+	}
+});
+*/
+function modalConfirm(confrimMessage) {
+	const CONFIRM_MODAL_DIV = document.createElement("div");
+	const CONFIRM_MODAL_DIALOG_DIV = document.createElement("div");
+	const CONFIRM_MODAL_CONTENT_DIV = document.createElement("div");
+	const CONFIRM_MODAL_HEADER_DIV = document.createElement("div");
+	const CONFIRM_MODAL_BODY_DIV = document.createElement("div");
+	const CONFIRM_MODAL_FOOTER_DIV = document.createElement("div");
+	const CONFIRM_MODAL_TITLE_H5 = document.createElement("h5");
+	const CONFIRM_MODAL_ACTION_BTN = document.createElement("button");
+	const CONFIRM_MODAL_CLOSE_BTN_1 = document.createElement("button");
+	const CONFIRM_MODAL_CANCLE_BTN = document.createElement("button");
+
+	setModalWrap(CONFIRM_MODAL_DIV,"confirmModal", "confirmModalTitle");
+	setModalContent(CONFIRM_MODAL_DIALOG_DIV, CONFIRM_MODAL_CONTENT_DIV)
+	setModalHeader(CONFIRM_MODAL_HEADER_DIV, CONFIRM_MODAL_TITLE_H5, "confirmModalTitle", CONFIRM_MODAL_CLOSE_BTN_1)
+	
+	CONFIRM_MODAL_HEADER_DIV.appendChild(CONFIRM_MODAL_TITLE_H5);
+	CONFIRM_MODAL_HEADER_DIV.appendChild(CONFIRM_MODAL_CLOSE_BTN_1);
+
+	setModalBody(CONFIRM_MODAL_BODY_DIV, confrimMessage)
+	
+	let buttonOptrions = [
+		{	
+			element : CONFIRM_MODAL_ACTION_BTN,
+			className : "btn btn-outline-primary",
+			textContent:"확인",
+			dismiss : false
+		},{
+			element : CONFIRM_MODAL_CANCLE_BTN,
+			className : "btn btn-outline-secondary",
+			textContent: "취소",
+			dismiss : true
+		}
+	]
+	
+	setModalFooter(CONFIRM_MODAL_FOOTER_DIV,buttonOptrions)
+	
+	CONFIRM_MODAL_FOOTER_DIV.appendChild(CONFIRM_MODAL_ACTION_BTN);
+	CONFIRM_MODAL_FOOTER_DIV.appendChild(CONFIRM_MODAL_CANCLE_BTN);
+	
+	CONFIRM_MODAL_CONTENT_DIV.appendChild(CONFIRM_MODAL_HEADER_DIV);
+	CONFIRM_MODAL_CONTENT_DIV.appendChild(CONFIRM_MODAL_BODY_DIV);
+	CONFIRM_MODAL_CONTENT_DIV.appendChild(CONFIRM_MODAL_FOOTER_DIV);
+	
+	CONFIRM_MODAL_DIALOG_DIV.appendChild(CONFIRM_MODAL_CONTENT_DIV);
+	
+	CONFIRM_MODAL_DIV.appendChild(CONFIRM_MODAL_DIALOG_DIV);
+	
+	document.body.appendChild(CONFIRM_MODAL_DIV);
+	feather.replace();
+	
+	$(CONFIRM_MODAL_DIV).modal("show");
+
+	function confirmPromise()
+	{
+		return new Promise((resolve) => {
+			CONFIRM_MODAL_CLOSE_BTN_1.addEventListener("click", function() {
+				CONFIRM_MODAL_DIV.remove();
+				resolve(false);
+			});
+			CONFIRM_MODAL_CANCLE_BTN.addEventListener("click", function() {
+				CONFIRM_MODAL_DIV.remove();
+				resolve(false);
+			});
+			CONFIRM_MODAL_ACTION_BTN.addEventListener("click", function() {
+				$(CONFIRM_MODAL_DIV).modal("hide");
+				CONFIRM_MODAL_DIV.remove();
+				resolve(true);
+			});
+			CONFIRM_MODAL_DIV.addEventListener("keyup", function(e) {
+				if (e.keyCode === 13) {
+					$(CONFIRM_MODAL_DIV).modal("hide");
+					CONFIRM_MODAL_DIV.remove();
+					resolve(true);
+				}
+			});
+		});
+	}
+	return confirmPromise();
+}
+
+/* ---- alert 알림 함수 ---- */
+/*
+사용예시)
+modalAlert("메시지");
+*/
+/**
+ *	alert 동적 생성 함수
+ *	@param alertMessage 알림글 메시지
+**/
+function modalAlert(alertMessage)
+{
+	const ALERT_MODAL_DIV = document.createElement("div");
+	const ALERT_MODAL_DIALOG_DIV = document.createElement("div");
+	const ALERT_MODAL_CONTENT_DIV = document.createElement("div");
+	const ALERT_MODAL_HEADER_DIV = document.createElement("div");
+	const ALERT_MODAL_BODY_DIV = document.createElement("div");
+	const ALERT_MODAL_FOOTER_DIV = document.createElement("div");
+	const ALERT_MODAL_TITLE_H5 = document.createElement("h5");
+	const ALERT_MODAL_CLOSE_BTN = document.createElement("button");
+	const ALERT_MODAL_BTN = document.createElement("button");
+	
+	setModalWrap(ALERT_MODAL_DIV, "alertModal", "alertModalTitle");
+	setModalContent(ALERT_MODAL_DIALOG_DIV, ALERT_MODAL_CONTENT_DIV)
+	setModalHeader(ALERT_MODAL_HEADER_DIV, ALERT_MODAL_TITLE_H5, "alertModalTitle", ALERT_MODAL_CLOSE_BTN)
+
+	ALERT_MODAL_HEADER_DIV.appendChild(ALERT_MODAL_TITLE_H5);
+	ALERT_MODAL_HEADER_DIV.appendChild(ALERT_MODAL_CLOSE_BTN);
+
+	setModalBody(ALERT_MODAL_BODY_DIV, alertMessage)
+
+	let buttonOptrions = [
+		{
+			element: ALERT_MODAL_BTN,
+			className: "btn btn-outline-primary customAlert",
+			textContent: "확인",
+			dismiss: false
+		}
+	]
+
+	setModalFooter(ALERT_MODAL_FOOTER_DIV, buttonOptrions)
+
+	ALERT_MODAL_FOOTER_DIV.appendChild(ALERT_MODAL_BTN);
+
+	ALERT_MODAL_CONTENT_DIV.appendChild(ALERT_MODAL_HEADER_DIV);
+	ALERT_MODAL_CONTENT_DIV.appendChild(ALERT_MODAL_BODY_DIV);
+	ALERT_MODAL_CONTENT_DIV.appendChild(ALERT_MODAL_FOOTER_DIV);
+
+	ALERT_MODAL_DIALOG_DIV.appendChild(ALERT_MODAL_CONTENT_DIV);
+
+	ALERT_MODAL_DIV.appendChild(ALERT_MODAL_DIALOG_DIV);
+
+	document.body.appendChild(ALERT_MODAL_DIV);
+	feather.replace();
+
+	$(ALERT_MODAL_DIV).modal("show");
+	
+	function alertPromise()
+	{
+		return new Promise(() => {
+			ALERT_MODAL_CLOSE_BTN.addEventListener("click", function() {
+				ALERT_MODAL_DIV.remove();
+			});
+			ALERT_MODAL_BTN.addEventListener("click", function() {
+				$(ALERT_MODAL_DIV).modal("hide");
+				ALERT_MODAL_DIV.remove();
+			});
+			ALERT_MODAL_DIV.addEventListener("keyup", function(e) {
+				if(e.keyCode === 13){
+					$(ALERT_MODAL_DIV).modal("hide");
+					ALERT_MODAL_DIV.remove();
+				}
+			});
+		});
+	}
+
+	return alertPromise();
+}
+/* ---- alert 알림 함수 끝 ---- */
+
+/* modal 동적 생성 */
+function setModalWrap(wrapDiv, modalId, modalTitleId)
+{
+	wrapDiv.className = "modal fade";
+	wrapDiv.id = modalId;
+	wrapDiv.tabIndex = "-1";
+	wrapDiv.role = "dialog";
+	wrapDiv.setAttribute("aria-labelledby", modalTitleId);
+	wrapDiv.setAttribute("aria-hidden", "true");
+	wrapDiv.setAttribute("data-bs-backdrop", "static");
+}
+
+function setModalContent(dialogDiv, contentDiv){
+	dialogDiv.className = "modal-dialog modal-dialog-centered";
+	dialogDiv.role = "document";
+	contentDiv.className = "modal-content";
+}
+
+function setModalHeader(headerDiv, titleH5, titleH5Id, button){
+	headerDiv.className = "modal-header";
+	
+	titleH5.className = "modal-title";
+	titleH5.id = titleH5Id;
+	titleH5.innerHTML = `<i class="text-warning" data-feather="bell"></i>VOVIC&E 알림`;
+	
+	button.className = "btn-close";
+	button.type = "button";
+	button.setAttribute("data-bs-dismiss", "modal");
+	button.setAttribute("aria-label", "Close");
+}
+
+function setModalBody(bodyDiv, message){
+	bodyDiv.className = "modal-body";
+	bodyDiv.innerHTML = message;
+}
+
+function setModalFooter(footerDiv, buttonOptrions){
+	footerDiv.className = "modal-footer";
+	for(let i=0; i < buttonOptrions.length; i++){
+		buttonOptrions[i].element.className = buttonOptrions[i].className;
+		buttonOptrions[i].element.type = "button";
+		buttonOptrions[i].element.textContent = buttonOptrions[i].textContent;
+		if(buttonOptrions[i].dismiss){
+			buttonOptrions[i].element.setAttribute("data-bs-dismiss", "modal");
+		}
+	}
+}
+/* modal 동적 생성 끝 */
+
+
 window.addEventListener("load", addActiveLnbArea);
 window.addEventListener("resize", addActiveLnbArea);
 feather.replace();
--- src/main/resources/static/vovi/reviewEvent/js/attendEvent.js
+++ src/main/resources/static/vovi/reviewEvent/js/attendEvent.js
@@ -16,6 +16,15 @@ let btnCount = 0;
 
 /* 함수 */
 
+function fn_callAddressApi(){
+	new daum.Postcode({
+		oncomplete: function(data) {
+			// 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
+			// 예제를 참고하여 다양한 활용법을 확인해 보세요.
+		}
+	}).open();
+}
+
 function fn_get(url, memberData, detail=null){
 	$.ajax({
 		url: contextPath + url,
@@ -43,27 +52,47 @@ function fn_showAddressView(){
 
 function fn_createModBtn(){
 	const MOD_BTN = `<button type="button" class="btn btn-outline-warning modBtn">수정</button>`;
-	$("#reviewEventCard").show();
-	$("#reviewEventCard").focus();
+	/*$("#reviewEventCard").show();
+	$("#reviewEventCard").focus();*/
 	$("#reviewerInfoCard .saveBtn").remove();
 	$("#reviewerInfoCard .card-header").append(MOD_BTN);
-	$("#reviewerInfoCard input").attr("readOnly", true);
+	$("#reviewerInfoCard input").prop("readOnly", true);
+	$("#findPostCodeBtn").prop("disabled", true);
 }
 
 function fn_createSaveBtn(){
 	const SAVE_BTN = `<button type="button" class="btn btn-outline-success saveBtn">저장</button>`;
 	$("#reviewEventCard").hide();
 	$("#reviewerInfoCard .modBtn").remove();
-	$("#reviewerInfoCard input").attr("readOnly", false);
 	$("#reviewerInfoCard .card-header").append(SAVE_BTN);
+	$("#reviewerInfoCard input").prop("readOnly", false);
+	$("#findPostCodeBtn").prop("disabled", false);
+	$("#findPostCodeBtn").off("click", fn_showAddressView).on("click", fn_showAddressView);
 }
 
 $('#reviewerInfoCard').on('click', '.saveBtn', function() {
-	fn_createModBtn();
+	modalConfirm("저장하시겠습니까?").then((result) => {
+		if (result) {
+			fn_createModBtn();
+			modalAlert("저장되었습니다.");
+			$(".customAlert").on("click", function(){
+				$("#reviewEventCard").show();
+				$("#reviewEventCard").focus();
+			})
+		}else{
+			modalAlert("저장이 취소되었습니다.");
+		}
+	});
 });
 
 $('#reviewerInfoCard').on('click', '.modBtn', function() {
-	fn_createSaveBtn();
+	modalConfirm(`<p>수정하시겠습니까?</p>`).then((result) => {
+		if (result) {
+			fn_createSaveBtn();
+		}else{
+			modalAlert("수정이 취소되었습니다.");
+		}
+	});
 });
 
 $('#userAgreeCard').on('change', '#checkAgree', function() {
@@ -80,8 +109,39 @@ $('#userAgreeCard').on('change', '#checkAgree', function() {
 	}
 });
 
+$("#intermediateSave").click(function (){
+	modalAlert("저장되었습니다.");
+})
+
 $("#eventParticipation").click(function (){
-	alert("참여버튼")
+	modalAlert("참여 완료되었습니다. 소중한 리뷰 감사합니다.");
+})
+
+$("#reviewer").on("input", function () {
+	let reviewer = $("#reviewer").val();
+	reviewer = reviewer.replace(/[0-9]/g, '');
+	$(this).val(reviewer);
+})
+
+$("#reviewerPhoneNum").on("input", function () {
+	let phoneNum = $("#reviewerPhoneNum").val();
+	phoneNum = phoneNum.replace(/[^\d]/g, '');
+	
+	if (phoneNum.length >= 4) {
+		phoneNum = phoneNum.replace(/(\d{3})(\d{3,4})(\d{4})/, '$1-$2-$3');
+	}
+	
+	if (phoneNum.length > 13) {
+		phoneNum = phoneNum.substring(0, 13);
+	}
+	
+	$(this).val(phoneNum);
+})
+
+$("#reviewerEmail").on("input", function () {
+	let reviewerEmail = $("#reviewerEmail").val();
+	reviewerEmail = reviewerEmail.replace(/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/, '');
+	$(this).val(reviewerEmail);
 })
 
 /* 초기화 */
